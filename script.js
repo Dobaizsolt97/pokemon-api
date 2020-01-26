bf14c1320b68991fe4026508ac3d46338f67670e
Dobai Zsolt <56483870+Dobaizsolt97@users.noreply.github.com>
2020-01-26 21:57:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ let input = document.querySelector(".search");
 let button = document.querySelector("#finder");
 let image = document.getElementById("poke-img");
 let span = document.getElementById("poke-name");
+let span1 = document.getElementById("poke-type");
 let right = document.getElementById("right");
 let left = document.getElementById("left");
 let up = document.getElementById("up");
 let down = document.getElementById("down");
 let redBtn = document.getElementById("red-btn");
+let greenBtn = document.getElementById("blk-btn");
+let container = document.querySelector(".container");
 let called = false;
+const body = document.querySelector("body");
 let pokemoncalledid;
 button.addEventListener("click", async () => {
-  let pokemon = input.value;
+  let pokemon = input.value.toLocaleLowerCase();
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
   let response = await fetch(url);
   let data = await response.json();
@@ -21,17 +25,17 @@ button.addEventListener("click", async () => {
     img: [data.sprites["front_default"], data.sprites["back_default"]],
     type: data.types.map(type => type.type.name).join(",")
   };
-  console.log(pokeentry);
+
   image.innerHTML = `<img class='inserted' src='${pokeentry.img[0]}'></img>`;
-  span.textContent = `${pokeentry.name}
-  ${pokeentry.type}`;
+  span.innerHTML = `<h3>${pokeentry.name}</h3>
+  <p>${pokeentry.type}</p>`;
   called = true;
   pokemoncalledid = pokeentry.id;
 });
 
 right.addEventListener("click", async () => {
   if (called) {
-    let pokemon = input.value;
+    let pokemon = pokemoncalledid;
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
     let response = await fetch(url);
     let data = await response.json();
@@ -48,7 +52,7 @@ right.addEventListener("click", async () => {
 });
 left.addEventListener("click", async () => {
   if (called) {
-    let pokemon = input.value;
+    let pokemon = pokemoncalledid;
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
     let response = await fetch(url);
     let data = await response.json();
@@ -78,8 +82,9 @@ up.addEventListener("click", async () => {
     };
     console.log(pokeentry);
     image.innerHTML = `<img class='inserted' src='${pokeentry.img[0]}'></img>`;
-    span.textContent = `${pokeentry.name}
-  ${pokeentry.type}`;
+    span.innerHTML = `<h3>${pokeentry.name}</h3>
+  <p>${pokeentry.type}</p>
+  `;
     pokemoncalledid += 1;
   }
 });
@@ -98,8 +103,9 @@ down.addEventListener("click", async () => {
     };
     console.log(pokeentry);
     image.innerHTML = `<img class='inserted' src='${pokeentry.img[0]}'></img>`;
-    span.textContent = `${pokeentry.name}
-  ${pokeentry.type}`;
+    span.innerHTML = `<h3>${pokeentry.name}</h3>
+  <p>${pokeentry.type}</p>
+  `;
     pokemoncalledid -= 1;
   }
 });
@@ -117,7 +123,34 @@ redBtn.addEventListener("click", async () => {
   };
   console.log(pokeentry);
   image.innerHTML = `<img class='inserted' src='${pokeentry.img[0]}'></img>`;
-  span.textContent = `${pokeentry.name}
-    ${pokeentry.type}`;
-  pokemoncalledid -= 1;
+  span.innerHTML = `<h3>${pokeentry.name}</h3>
+  <p>${pokeentry.type}</p>
+    `;
+
+  pokemoncalledid = pokeentry.id;
+  called = true;
 });
+greenBtn.addEventListener("click", async () => {
+  if (called) {
+    let pokemon = pokemoncalledid;
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    const pokeentry = {
+      name: data.name,
+      id: data.id,
+      img: [data.sprites["front_default"], data.sprites["back_default"]],
+      type: data.types.map(type => type.type.name).join(","),
+      weight: data.weight / 10,
+      height: data.height * 0.08
+    };
+    //span.textContent = `${(pokeentry.weight * 0.45).toFixed(0)} kilograms
+    //${(pokeentry.height * 0.3).toFixed(1)} meters tall`;
+    span.innerHTML = `<h3 class='greeninsert'> ${pokeentry.weight.toFixed(
+      1
+    )}gk</h3>
+  <p class='greeninsert'>${pokeentry.height.toFixed(1)}m</p>`;
+  }
+});
+
